4b00ec6139eabc39166b36de806e95b168013619
Aknik <kinka@tutanota.de>
2017-04-03 20:46:56 +0200

    Update ssh.py
    Con la idea de hacer un bot calculadora aprovechando la potencia de PYTHON
    Ahora por defecto interpreta la orden en python como argumento que se envia a PRINT
    por ejemplo :
    > pi*e (calcula pi por e)
    > 3**2 (3 elevado a 2)
    > sin(radianes(45))  (convierte 45 a radianes y calcula el seno)
    Se puede seguir usando para enviar comandos a la shell en bash, anteponiendo / a la instruccion
    /ls (ls lista directorio)
    /cat fichero 
    etc ...

--- ssh.py
+++ ssh.py
@@ -1,21 +1,21 @@
+
 # -*- coding: utf-8 -*-
+import telebot 
+from telebot import types 
+import time 
+import subprocess 
 
-import telebot
-from telebot import types
-import time
-import subprocess
-
-
-TOKEN = "<Token>"
-admin = <ID Admin>
-
-bot = telebot.TeleBot(TOKEN)
+TOKEN = "-------------------------------"
+admin = 6660666
+bot = telebot.TeleBot(TOKEN) 
 
 def listener(messages):
     for m in messages:
+        #print m
         if m.from_user.id == admin:
             if m.content_type == 'text':
                 if m.text.startswith('get'):
+                    # Si el comando empieza por get , envia el fichero indicado get:fichero 
                     pwd = subprocess.Popen('pwd', shell=True, stdout=subprocess.PIPE).stdout.read()
                     pwd = pwd[0:len(pwd)-1]
                     try:
@@ -24,14 +24,23 @@ def listener(messages):
                         bot.send_message( admin, "Error enviando el documento")
                 else:
                     execute_command(m)
-                print str(m.from_user.first_name) + " [" + str(m.chat.id) + "]: " + m.text
+                print str(m.from_user.first_name) + " [" + str(m.chat.id) + "]: " + m.text 
 
-bot.set_update_listener(listener)
-
+bot.set_update_listener(listener) 
 
 def execute_command(message):
     cid = message.chat.id
+    
+    if message.text.startswith('/'):
+        # Si el texto empieza por / lo considera comando bash y en caso contrario, python
+        message.text = message.text[1:]
+        print message.text
+    else:
+        # Se le envia el comando como instruccion a calcular en python
+        message.text = "python2 -c 'from math import *; print " + message.text + "'" 
+    
     result_command = subprocess.Popen(message.text, shell=True, stdout=subprocess.PIPE).stdout.read()
+#    print result_command
     try:
         bot.send_message( cid, result_command)
     except:
@@ -41,11 +50,11 @@ def execute_command(message):
     
     if exception:
         if result_command == "":
-            bot.send_message( cid, "Empty output")
+            bot.send_message( cid, "?")
         else:
             with open( 'tmp.txt', 'w') as f:
                 f.write( result_command)
             bot.send_document( cid, open( 'tmp.txt', 'rb'))
-
-
 bot.polling(none_stop=True)
+
+	
